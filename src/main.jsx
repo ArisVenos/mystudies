a96e1dcc2e53ab1c72206d683c3bdbdbc54da3be
sdi2000266 <sdi2000266@sdi.uoa.gr>
2023-12-20 00:12:42 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+ReactDOM.render(<App />, document.getElementById('root'));
+
+const App = () => {
+  return (
+    <div>
+      <button>Click me!</button>
+    </div>
+  );
+};
+
+export default App;
