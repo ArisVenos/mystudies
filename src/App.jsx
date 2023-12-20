c768c02d8a8db98fe0ae823848ec984d2f082262
DimitrisLyk <dlykour@gmail.com>
2023-12-20 23:18:28 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react';
-
+import logo from './logo.png';
 function App() {
     const [count, setCount] = useState(0);
 
@@ -11,7 +11,7 @@ function App() {
     return (
         <div>
             <div style={{ display: 'flex', alignItems: 'center', borderBottom: '1px solid black' }}>
-                <img src="./logo.png" alt="Logo" style={{ marginRight: 'auto' }} />
+            <img src={logo} alt="Logo" style={{ marginRight: 'auto' }} />
                 <Button colorScheme="green" onClick={incrementCount}>Click me</Button>
             </div>
             <p>Count: {count}</p>
