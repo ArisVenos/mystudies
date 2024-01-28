f24a7bb6ee91d7ae9722698f6368441759a7f80d
DimitrisLyk <dlykour@gmail.com>
2024-01-28 19:16:19 +0200

    okk

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -33,6 +33,8 @@ const CourseApplication = ({ db, userCourses }) => {
   const [message, setMessage] = useState('');
   const [selectedCourseInfo, setSelectedCourseInfo] = useState(null);
   const [isCourseInfoModalOpen, setIsCourseInfoModalOpen] = useState(false);
+  const [coursesForConfirmation, setCoursesForConfirmation] = useState([]);
+
 
   useEffect(() => {
     const fetchCourses = async () => {
@@ -53,6 +55,47 @@ const CourseApplication = ({ db, userCourses }) => {
 
     fetchCourses();
   }, [db]);
+
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+  const handleOpenConfirmModal = async () => {
+    const userEmail = localStorage.getItem('email');
+    const userCoursesCollection = collection(db, 'users');
+    const userCourseRef = doc(userCoursesCollection, userEmail);
+  
+    try {
+      const userCourseDoc = await getDoc(userCourseRef);
+  
+      if (userCourseDoc.exists()) {
+        const userData = userCourseDoc.data();
+        const userCourses = userData.courses || [];
+  
+        console.log("All User Courses: ", userCourses); // Debugging line
+  
+        const coursesForConfirmation = userCourses.filter((course) => course.declared === 1);
+  
+        console.log("Courses for Confirmation: ", coursesForConfirmation);
+  
+        if (coursesForConfirmation.length > 0) {
+          setCoursesForConfirmation(coursesForConfirmation); // Update the state with the complete course information
+          setIsConfirmModalOpen(true);
+        } else {
+          setMessage('Δεν έχετε επιλέξει κανένα μάθημα για οριστικοποίηση.');
+        }
+      } else {
+        console.error('User document does not exist.');
+      }
+    } catch (error) {
+      console.error('Error fetching user courses:', error.message);
+    }
+  };
+
+  
+
+  const handleCloseConfirmModal = () => {
+    setIsConfirmModalOpen(false);
+  };
+
 
   const handleCheckboxChange = (courseTitle) => {
     setSelectedCourses((prevSelectedCourses) => {
@@ -89,7 +132,7 @@ const CourseApplication = ({ db, userCourses }) => {
         const appliedCourses = userData.courses || [];
   
         const alreadyAppliedCourses = selectedCourses.filter((course) =>
-          appliedCourses.some((appliedCourse) => appliedCourse.title === course && appliedCourse.declared === 1)
+          appliedCourses.some((appliedCourse) => appliedCourse.title === course && (appliedCourse.declared === 1 || appliedCourse.declared === 10))
         );
   
         if (alreadyAppliedCourses.length > 0) {
@@ -142,7 +185,43 @@ const CourseApplication = ({ db, userCourses }) => {
       setMessage('Σφάλμα κατά την υποβολή αίτησης. Παρακαλώ προσπαθήστε ξανά.');
     }
   };
+
+  const handleApplyAll = async () => {
+    try {
+      const userEmail = localStorage.getItem('email');
   
+      // Get the current user's declared courses
+      const userCoursesCollection = collection(db, 'users');
+      const userCourseRef = doc(userCoursesCollection, userEmail);
+      const userCourseDoc = await getDoc(userCourseRef);
+  
+      if (userCourseDoc.exists()) {
+        const userData = userCourseDoc.data();
+        const declaredCourses = userData.courses || [];
+  
+        // Update the 'declared' property to 10 for declared courses
+        const updatedCourses = courses.map((course) => {
+          if (declaredCourses.some((declaredCourse) => declaredCourse.title === course.title && declaredCourse.declared === 1)) {
+            return { ...course, declared: 10 }; // Change the declared value to 10 for permanent declaration
+          } else {
+            return course;
+          }
+        });
+  
+        // Update the 'users' document with the permanently declared courses
+        await updateDoc(userCourseRef, {
+          courses: updatedCourses,
+        });
+  
+        setMessage('Η μόνιμη δήλωση μαθημάτων ολοκληρώθηκε με επιτυχία');
+      }
+    } catch (error) {
+      console.error('Error applying for all courses:', error.message);
+      setMessage('Σφάλμα κατά τη μόνιμη δήλωση όλων των μαθημάτων. Παρακαλώ προσπαθήστε ξανά.');
+    } finally {
+      setIsConfirmModalOpen(false);
+    }
+  };
 
   return (
     <Center>
@@ -188,7 +267,39 @@ const CourseApplication = ({ db, userCourses }) => {
         <Button colorScheme="teal" onClick={handleApply} disabled={selectedCourses.length === 0}>
           ΔΗΛΩΣΗ
         </Button>
+
       </VStack>
+
+                
+        <Box bg="#26abcc" borderBottom="4px solid #4f4f50" position="absolute" top={210} right={20}>
+          <Button bg="#26abcc" color="white" onClick={handleOpenConfirmModal} disabled={coursesForConfirmation.length === 0}>
+            ΟΡΙΣΤΙΚΟΠΟΙΗΣΗ
+          </Button>
+
+        </Box>
+        <Modal isOpen={isConfirmModalOpen} onClose={handleCloseConfirmModal}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader background='#26abcc' color='white'>Επιβεβαίωση Δήλωσης</ModalHeader>
+            <ModalCloseButton />
+              <ModalBody>
+                <Text>Είστε σίγουροι ότι θέλετε να οριστικοποιησετε τα μαθήματα:</Text>
+                {coursesForConfirmation.map((course, index) => (
+                  <Box key={index} mt={2}>
+                    <Text>{course.title}</Text>
+                  </Box>
+                ))}
+              </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="teal" onClick={handleApplyAll}>Επιβεβαίωση</Button>
+              <Button variant="ghost" onClick={handleCloseConfirmModal}>Ακύρωση</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+
+
+
       <Box bg="#26abcc" borderBottom="4px solid #4f4f50" position="absolute" top={210} left={20}>
         <Button bg="#26abcc" color="white" onClick={handleHistoryClick}>
           ΙΣΤΟΡΙΚΟ ΔΗΛΩΣΕΩΝ
@@ -226,4 +337,3 @@ const CourseApplication = ({ db, userCourses }) => {
 };
 
 export default CourseApplication;
-
